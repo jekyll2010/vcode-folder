34d1bb611ff3924da6902f7ad757494526ffe2da
John <jekyll.2010@icloud.com>
2022-10-01 12:37:37 -0230

    first draft

---
+++ lottery1.py
@@ -0,0 +1,14 @@
+
+import random
+
+
+list=[]    
+for i in range(0,6):
+    list.append ( random.randint(1,49))
+list.sort()
+print (list)
+
+
+    
+    
+    
---
+++ lottery2.py
@@ -0,0 +1,13 @@
+
+import random
+
+
+list=(random.sample(range(49),6))
+list.sort()
+print (list)
+
+
+
+    
+    
+    
